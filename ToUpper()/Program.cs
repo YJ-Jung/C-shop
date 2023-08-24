9a2fd78ce236894c15bab2218e23d723568dd25a
YJ-Jung <133944162+YJ-Jung@users.noreply.github.com>
2023-08-24 14:29:13 +0900

    ToUpper() method

---
+++ ToUpper()/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToUpper__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. if you want to make the alphabets of the string as all capital, using ToUpper() method
+            string alphabet = "abcdef";
+            string upperResult = alphabet.ToUpper();
+
+            Console.WriteLine($"upperResult : {upperResult}");
+
+            // 2. if you want to make the alphabets of the string as all lower case , using ToLower() method
+            string alphabet2 = "ABCDEF";
+            string lowerResult = alphabet2.ToLower();
+
+            Console.WriteLine($"lowerResult : {lowerResult}");
+
+            // 3. 
+        }
+    }
+}
